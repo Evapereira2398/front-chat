82ffacc9d3848f3d22a7ed555f4f891ad1d8e5be
Evanderson <92355749+Evapereira2398@users.noreply.github.com>
2023-05-30 05:05:09 -0300

    Indentação

--- src/components/Kanban/KanbanBoard/KanbanBoard.jsx
+++ src/components/Kanban/KanbanBoard/KanbanBoard.jsx
@@ -18,17 +18,16 @@ const KanbanBoard = ( { chatlist } ) => {
         { id: 2, content: "Task 2" },
       ],
     },
+
     {
       id: 2,
       title: "In Progress",
       cards: [
         { id: 3, content: "Task 3" },
         { id: 14, content: "Task 14" },
-        { id: 15, content: "Task 15" },
-        { id: 16, content: "Task 16" },
-        { id: 17, content: "Task 17" },
       ],
     },
+
     {
       id: 3,
       title: "Done",
@@ -36,36 +35,30 @@ const KanbanBoard = ( { chatlist } ) => {
         { id: 4, content: "Task 4" },
         { id: 5, content: "Task 5" },
         { id: 6, content: "Task 6" },
-        { id: 7, content: "Task 7" },
-        { id: 8, content: "Task 8" },
-        { id: 10, content: "Task 10" },
-        { id: 11, content: "Task 11" },
-        { id: 12, content: "Task 12" },
-        { id: 13, content: "Task 13" },
       ],
     },
+
   ]);
 
 const [editingColumnId, setEditingColumnId] = useState(null);
 const [newColumnTitle, setNewColumnTitle] = useState("");
 const [showConfirmationBox, setShowConfirmationBox] = useState(false);
 const [columnToDeleteId, setColumnToDeleteId] = useState(null);
-
-
 const [showAddCardDialog, setShowAddCardDialog] = useState(false);
 const [selectedChatId, setSelectedChatId] = useState(null);
 
+// Função para adicionar novos cards nas colunas
 const handleAddCard = (columnId) => {
   setSelectedChatId(columnId);
   setShowAddCardDialog(true);
 };
 
-
+// Função para selecionar o chat que se tornrá um card
 const handleSelectChat = (chatId) => {
   setSelectedChatId(chatId);
 };
 
-
+// Função para confirmar a adição de um novo card 
 const handleConfirmAddCard = () => {
   if (selectedChatId !== null) {
     const newCard = {
@@ -89,6 +82,7 @@ const handleConfirmAddCard = () => {
   }
 };
 
+// Função para cancelar a adição de um novo card
 const handleCancelAddCard = () => {
   setShowAddCardDialog(false);
 };
@@ -112,11 +106,13 @@ const handleAddColumn = () => {
   setColumns(updatedColumns);
 };
 
+// Função para editar o nome de uma coluna
 const handleEditColumnTitle = (columnId, currentTitle) => {
     setEditingColumnId(columnId);
     setNewColumnTitle(currentTitle);
 };
 
+// Função para salvar o novo nome da coluna
 const handleSaveColumnTitle = (columnId) => {
     const updatedColumns = columns.map((column) => {
       if (column.id === columnId) {
@@ -130,81 +126,91 @@ const handleSaveColumnTitle = (columnId) => {
     setNewColumnTitle("");
   };
 
-  const handleKeyDown = (e, columnId) => {
-    if (e.key === "Enter") {
-      handleSaveColumnTitle(columnId);
-    }
-  };
+// Função de clique na tecla enter para salvar o novo nome da coluna
+const handleKeyDown = (e, columnId) => {
+  if (e.key === "Enter") {
+    handleSaveColumnTitle(columnId);
+  }
+};
 
-  const handleDragStart = (e, columnId, cardId) => {
-    e.dataTransfer.setData("columnId", columnId);
-    e.dataTransfer.setData("cardId", cardId);
-  };
+// Função de Drag and drop (Arrastar e soltar) 
+const handleDragStart = (e, columnId, cardId) => {
+  e.dataTransfer.setData("columnId", columnId);
+  e.dataTransfer.setData("cardId", cardId);
+};
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
+// Função de Drag and drop (Arrastar e soltar) 
+const handleDragOver = (e) => {
+  e.preventDefault();
+};
 
-  const handleDrop = (e, targetColumnId) => {
-    e.preventDefault();
+// Função de Drag and drop (Arrastar e soltar)
+const handleDrop = (e, targetColumnId) => {
+  e.preventDefault();
 
-    const sourceColumnId = Number(e.dataTransfer.getData("columnId"));
-    const cardId = Number(e.dataTransfer.getData("cardId"));
+  const sourceColumnId = Number(e.dataTransfer.getData("columnId"));
+  const cardId = Number(e.dataTransfer.getData("cardId"));
 
-    const updatedColumns = [...columns];
+  const updatedColumns = [...columns];
 
-    const sourceColumnIndex = updatedColumns.findIndex(
-      (column) => column.id === sourceColumnId
-    );
+  const sourceColumnIndex = updatedColumns.findIndex(
+    (column) => column.id === sourceColumnId
+  );
 
-    const cardIndex = updatedColumns[sourceColumnIndex].cards.findIndex(
-      (card) => card.id === cardId
-    );
+  const cardIndex = updatedColumns[sourceColumnIndex].cards.findIndex(
+    (card) => card.id === cardId
+  );
 
-    const [draggedCard] = updatedColumns[sourceColumnIndex].cards.splice(
-      cardIndex,
-      1
-    );
+  const [draggedCard] = updatedColumns[sourceColumnIndex].cards.splice(
+    cardIndex,
+    1
+  );
 
-    const targetColumnIndex = updatedColumns.findIndex(
-      (column) => column.id === targetColumnId
-    );
+  const targetColumnIndex = updatedColumns.findIndex(
+    (column) => column.id === targetColumnId
+  );
 
-    updatedColumns[targetColumnIndex].cards.push(draggedCard);
+  updatedColumns[targetColumnIndex].cards.push(draggedCard);
 
-    setColumns(updatedColumns);
-  };
+  setColumns(updatedColumns);
+};
 
-  const handleDeleteColumn = (columnId) => {
-    setColumnToDeleteId(columnId);
-    setShowConfirmationBox(true);
-  };
+// Função para deletar coluna
+const handleDeleteColumn = (columnId) => {
+  setColumnToDeleteId(columnId);
+  setShowConfirmationBox(true);
+};
 
-  const confirmDeleteColumn = (columnId) => {
-    const updatedColumns = columns.filter((column) => column.id !== columnId);
+// Função para confirmar a exclusão da coluna
+const confirmDeleteColumn = (columnId) => {
+  const updatedColumns = columns.filter((column) => column.id !== columnId);
     setColumns(updatedColumns);
     setColumnToDeleteId(null);
     setShowConfirmationBox(false);
   };
 
-  const cancelDeleteColumn = () => {
+// Função para cancelar a exclusão das colunas
+const cancelDeleteColumn = () => {
     setColumnToDeleteId(null);
     setShowConfirmationBox(false);
   };
 
-  const handleSelectColumn = (columnId) => {
-    setSelectedChatId(columnId);
-  };
+
+// Função para selecionar a coluna via ID
+const handleSelectColumn = (columnId) => {
+  setSelectedChatId(columnId);
+};
   
 
-  return (
-    <div>
-      <KanbanHeader onAddColumn={handleAddColumn}/>
+return (
+  <div>
+    <KanbanHeader onAddColumn={handleAddColumn}/>
 
       <div className="kanban-board">
         {columns.map((column) => (
+
           <div className="column" key={column.id}>
-            <div className="column-header">
+            <div className="column-header"> 
               <h3 className="column-title">
                 {editingColumnId === column.id ? (
                   <div className="column-title-input">
@@ -214,23 +220,22 @@ const handleSaveColumnTitle = (columnId) => {
                       onChange={(e) => setNewColumnTitle(e.target.value)}
                       onKeyDown={(e) => handleKeyDown(e, column.id)}
                     />
-                    <label>Nome Coluna</label>
+                    <label> Nome Coluna </label>
                     <span className="input-highlight"></span>
                   </div>
-                ) : (
+                  ) : (
                   <div className="column-title-name"> {column.title} </div>
                 )}
               </h3>
 
               <div className="column-edit">
                 {editingColumnId === column.id ? (
-                  <button
-                    className="save-button"
-                    onClick={() => handleSaveColumnTitle(column.id)}
-                  >
+                  <button className="save-button" onClick={() => handleSaveColumnTitle(column.id)}>
                     Salvar
                   </button>
-                ) : (
+                  ) : (
+
+                  // Icones das colunas 
                   <div className="icons-container">
                     <Tooltip title="Editar nome">
                       <CreateIcon
@@ -287,16 +292,10 @@ const handleSaveColumnTitle = (columnId) => {
               <div className="confirmation-box-content">
                 <p>Tem certeza que deseja excluir a coluna?</p>
                 <div className="confirmation-box-buttons">
-                  <button
-                    className="confirmation-box-button"
-                    onClick={() => confirmDeleteColumn(columnToDeleteId)}
-                  >
+                  <button className="confirmation-box-button" onClick={() => confirmDeleteColumn(columnToDeleteId)}>
                     Sim
                   </button>
-                  <button
-                    className="confirmation-box-button"
-                    onClick={cancelDeleteColumn}
-                  >
+                  <button className="confirmation-box-button" onClick={cancelDeleteColumn}>
                     Não
                   </button>
                 </div>
@@ -305,41 +304,43 @@ const handleSaveColumnTitle = (columnId) => {
           </div>
         )};
 
-{showAddCardDialog && (
-  <div className="dialog-overlay">
-    <div className="dialog-container">
-      <div className="add-card-dialog">
-        <h3>Selecione uma conversa: </h3>
-          <div className="chat-list">
-            {chatlist.map((chat) => (
-              <div 
-                key={chat.chatId} 
-                className={`chatListItem ${selectedChatId === chat.chatId ? 'selected' : ''}`}
-                onClick={() => handleSelectChat(chat.chatId)}   
-              >
-                <img src={chat.image} alt="Avatar" className="chatListItem--avatar"/>
-                  <div className="chatListItem--lines">
-                    <div className="chatListItem--line">
-                      <div className="chatListItem--name">{chat.title}</div>
-                      <div className="chatListItem--date">19:00</div>
-                    </div>
-                      <div className="chatListItem--lastMsg">
-                        <p>Ultima mensagem</p>
+        {showAddCardDialog && (
+          <div className="dialog-overlay">
+            <div className="dialog-container">
+              <div className="add-card-dialog">
+                <h3>Selecione uma conversa: </h3>
+                  
+                  <div className="chat-list">
+                    {chatlist.map((chat) => (
+                      <div 
+                        key={chat.chatId} 
+                        className={`chatListItem ${selectedChatId === chat.chatId ? 'selected' : ''}`}
+                        onClick={() => handleSelectChat(chat.chatId)}   
+                      >
+                        <img src={chat.image} alt="Avatar" className="chatListItem--avatar"/>
+                          <div className="chatListItem--lines">
+                            <div className="chatListItem--line">
+                              <div className="chatListItem--name">{chat.title}</div>
+                              <div className="chatListItem--date">19:00</div>
+                            </div>
+                            
+                            <div className="chatListItem--lastMsg">
+                              <p>Ultima mensagem</p>
+                            </div>
+                          </div>
                       </div>
+                    ))}
+                  </div>
+
+                  <div className="dialog-buttons">
+                    <button onClick={handleConfirmAddCard}>Adicionar Card</button>
+                    <button onClick={handleCancelAddCard}>Cancelar</button>
                   </div>
               </div>
-            ))}
-        </div>
-      <div className="dialog-buttons">
-      <button onClick={handleConfirmAddCard}>Adicionar Card</button>
-      <button onClick={handleCancelAddCard}>Cancelar</button>
-    </div>
-      </div>
-    </div>
+            </div>
+          </div>
+        )}
   </div>
-  
-)}
-    </div>
   );
 };
 
